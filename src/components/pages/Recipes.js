6d69f956b14bba2f6c246318fd0fb5b6957c04a5
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-07 10:27:07 -0400

    finished

--- src/components/pages/Recipes.js
+++ src/components/pages/Recipes.js
@@ -22,18 +22,19 @@ export default class Recipes extends Component {
         try {
             const data = await fetch(this.state.url)
             const jsonData = await data.json()
-            console.log(jsonData)
+
             if (jsonData.recipes.length === 0) {
                 this.setState({
                     error: 'sorry but your search did not return any recipes, please try again or press search icon for the most popular recipes'
                 })
 
-            }
+            } else {
 
-            this.setState({
-                recipes: jsonData.recipes,
-                error: ''
-            })
+                this.setState({
+                    recipes: jsonData.recipes,
+                    error: ''
+                })
+            }
 
         } catch (error) {
             console.log(error)
@@ -73,7 +74,7 @@ export default class Recipes extends Component {
                     <section>
                         <div className="row">
                             <div className="col">
-                                <h2 className="text-orange text-center text-uppercase mt-5"></h2>
+                                <h2 className="text-orange text-center text-uppercase mt-5">{this.state.error}</h2>
                             </div>
                         </div>
                     </section>
